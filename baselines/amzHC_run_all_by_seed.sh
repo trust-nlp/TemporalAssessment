0f61f2d23e0241360e11f7ccb235c04164db430f
weisi <weisi2580@gmail.com>
2023-11-28 03:16:19 -0600

    changed amzHC_run_all_by_seed.sh: deleted

--- baselines/amzHC_run_all_by_seed.sh
+++ baselines/amzHC_run_all_by_seed.sh
@@ -4,7 +4,6 @@ TASK='amzReviewHC_rating'
 BATCH_SIZE=32
 ACCUMULATION_STEPS=2
 SPLITSEED=1
-LABEL= overall
 RUN_FILE='/home/weisi/Temporal/baselines/classification.py'
 BASE_PATH='/home/weisi/Temporal/data/Amazon/HealthCare'
 
@@ -17,7 +16,7 @@ CUDA_VISIBLE_DEVICES=${GPU_NUMBER} python ${RUN_FILE} --model_name_or_path ${MOD
     --validation_file  "$BASE_PATH/seed$SPLITSEED/amzHC_T1_2007-2010_validation.json"\
     --test_file  "$BASE_PATH/seed$SPLITSEED/amzHC_T1_2007-2010_test.json"\
     --evaluation_strategy epoch --save_strategy epoch --save_total_limit 2 --max_seq_length 512 --learning_rate 3e-5 --num_train_epochs 25 \
-    --shuffle_train_dataset --text_column_name text --label_column_name ${LABEL}  --do_train --do_eval --do_predict --load_best_model_at_end --metric_for_best_model micro_f1 --greater_is_better True \
+    --shuffle_train_dataset --text_column_name text --label_column_name overall  --do_train --do_eval --do_predict --load_best_model_at_end --metric_for_best_model micro_f1 --greater_is_better True \
     --per_device_train_batch_size ${BATCH_SIZE} --per_device_eval_batch_size ${BATCH_SIZE} --gradient_accumulation_steps ${ACCUMULATION_STEPS} --eval_accumulation_steps ${ACCUMULATION_STEPS}
 
 CUDA_VISIBLE_DEVICES=${GPU_NUMBER} python ${RUN_FILE} --model_name_or_path ${MODEL_NAME} --seed 42\
@@ -26,7 +25,7 @@ CUDA_VISIBLE_DEVICES=${GPU_NUMBER} python ${RUN_FILE} --model_name_or_path ${MOD
     --validation_file  "$BASE_PATH/seed$SPLITSEED/amzHC_T1_2007-2010_validation.json"\
     --test_file  "$BASE_PATH/seed$SPLITSEED/amzHC_T2_2011-2012_test.json"\
     --evaluation_strategy epoch --save_strategy epoch --save_total_limit 2 --max_seq_length 512 --learning_rate 3e-5 --num_train_epochs 25 \
-    --shuffle_train_dataset --text_column_name text --label_column_name ${LABEL}  --do_train --do_eval --do_predict --load_best_model_at_end --metric_for_best_model micro_f1 --greater_is_better True \
+    --shuffle_train_dataset --text_column_name text --label_column_name overall --do_train --do_eval --do_predict --load_best_model_at_end --metric_for_best_model micro_f1 --greater_is_better True \
     --per_device_train_batch_size ${BATCH_SIZE} --per_device_eval_batch_size ${BATCH_SIZE} --gradient_accumulation_steps ${ACCUMULATION_STEPS} --eval_accumulation_steps ${ACCUMULATION_STEPS}
 
 CUDA_VISIBLE_DEVICES=${GPU_NUMBER} python ${RUN_FILE} --model_name_or_path ${MODEL_NAME} --seed 42\
@@ -35,7 +34,7 @@ CUDA_VISIBLE_DEVICES=${GPU_NUMBER} python ${RUN_FILE} --model_name_or_path ${MOD
     --validation_file  "$BASE_PATH/seed$SPLITSEED/amzHC_T1_2007-2010_validation.json"\
     --test_file  "$BASE_PATH/seed$SPLITSEED/amzHC_T3_2013-2014_test.json"\
     --evaluation_strategy epoch --save_strategy epoch --save_total_limit 2 --max_seq_length 512 --learning_rate 3e-5 --num_train_epochs 25 \
-    --shuffle_train_dataset --text_column_name text --label_column_name ${LABEL} --do_train --do_eval --do_predict --load_best_model_at_end --metric_for_best_model micro_f1 --greater_is_better True \
+    --shuffle_train_dataset --text_column_name text --label_column_name overall --do_train --do_eval --do_predict --load_best_model_at_end --metric_for_best_model micro_f1 --greater_is_better True \
     --per_device_train_batch_size ${BATCH_SIZE} --per_device_eval_batch_size ${BATCH_SIZE} --gradient_accumulation_steps ${ACCUMULATION_STEPS} --eval_accumulation_steps ${ACCUMULATION_STEPS}
 
 CUDA_VISIBLE_DEVICES=${GPU_NUMBER} python ${RUN_FILE} --model_name_or_path ${MODEL_NAME} --seed 42\
@@ -44,7 +43,7 @@ CUDA_VISIBLE_DEVICES=${GPU_NUMBER} python ${RUN_FILE} --model_name_or_path ${MOD
     --validation_file  "$BASE_PATH/seed$SPLITSEED/amzHC_T2_2011-2012_validation.json"\
     --test_file  "$BASE_PATH/seed$SPLITSEED/amzHC_T2_2011-2012_test.json"\
     --evaluation_strategy epoch --save_strategy epoch --save_total_limit 2 --max_seq_length 512 --learning_rate 3e-5 --num_train_epochs 25 \
-    --shuffle_train_dataset --text_column_name text --label_column_name ${LABEL}  --do_train --do_eval --do_predict --load_best_model_at_end --metric_for_best_model micro_f1 --greater_is_better True \
+    --shuffle_train_dataset --text_column_name text --label_column_name overall  --do_train --do_eval --do_predict --load_best_model_at_end --metric_for_best_model micro_f1 --greater_is_better True \
     --per_device_train_batch_size ${BATCH_SIZE} --per_device_eval_batch_size ${BATCH_SIZE} --gradient_accumulation_steps ${ACCUMULATION_STEPS} --eval_accumulation_steps ${ACCUMULATION_STEPS}
 
 CUDA_VISIBLE_DEVICES=${GPU_NUMBER} python ${RUN_FILE} --model_name_or_path ${MODEL_NAME} --seed 42\
@@ -53,7 +52,7 @@ CUDA_VISIBLE_DEVICES=${GPU_NUMBER} python ${RUN_FILE} --model_name_or_path ${MOD
     --validation_file  "$BASE_PATH/seed$SPLITSEED/amzHC_T2_2011-2012_validation.json"\
     --test_file  "$BASE_PATH/seed$SPLITSEED/amzHC_T3_2013-2014_test.json"\
     --evaluation_strategy epoch --save_strategy epoch --save_total_limit 2 --max_seq_length 512 --learning_rate 3e-5 --num_train_epochs 25 \
-    --shuffle_train_dataset --text_column_name text --label_column_name ${LABEL}  --do_train --do_eval --do_predict --load_best_model_at_end --metric_for_best_model micro_f1 --greater_is_better True \
+    --shuffle_train_dataset --text_column_name text --label_column_name overall --do_train --do_eval --do_predict --load_best_model_at_end --metric_for_best_model micro_f1 --greater_is_better True \
     --per_device_train_batch_size ${BATCH_SIZE} --per_device_eval_batch_size ${BATCH_SIZE} --gradient_accumulation_steps ${ACCUMULATION_STEPS} --eval_accumulation_steps ${ACCUMULATION_STEPS}
 
 
@@ -63,7 +62,7 @@ CUDA_VISIBLE_DEVICES=${GPU_NUMBER} python ${RUN_FILE} --model_name_or_path ${MOD
     --validation_file  "$BASE_PATH/seed$SPLITSEED/amzHC_T3_2013-2014_validation.json"\
     --test_file  "$BASE_PATH/seed$SPLITSEED/amzHC_T3_2013-2014_test.json"\
     --evaluation_strategy epoch --save_strategy epoch --save_total_limit 2 --max_seq_length 512 --learning_rate 3e-5 --num_train_epochs 25 \
-    --shuffle_train_dataset --text_column_name text --label_column_name ${LABEL}  --do_train --do_eval --do_predict --load_best_model_at_end --metric_for_best_model micro_f1 --greater_is_better True \
+    --shuffle_train_dataset --text_column_name text --label_column_name overall  --do_train --do_eval --do_predict --load_best_model_at_end --metric_for_best_model micro_f1 --greater_is_better True \
     --per_device_train_batch_size ${BATCH_SIZE} --per_device_eval_batch_size ${BATCH_SIZE} --gradient_accumulation_steps ${ACCUMULATION_STEPS} --eval_accumulation_steps ${ACCUMULATION_STEPS}
 
 CUDA_VISIBLE_DEVICES=${GPU_NUMBER} python ${RUN_FILE} --model_name_or_path ${MODEL_NAME} --seed 42\
@@ -72,5 +71,5 @@ CUDA_VISIBLE_DEVICES=${GPU_NUMBER} python ${RUN_FILE} --model_name_or_path ${MOD
     --validation_file  "$BASE_PATH/seed$SPLITSEED/amzHC_AY_2007-2014_validation.json"\
     --test_file  "$BASE_PATH/seed$SPLITSEED/amzHC_T3_2013-2014_test.json"\
     --evaluation_strategy epoch --save_strategy epoch --save_total_limit 2 --max_seq_length 512 --learning_rate 3e-5 --num_train_epochs 25 \
-    --shuffle_train_dataset --text_column_name text --label_column_name ${LABEL}  --do_train --do_eval --do_predict --load_best_model_at_end --metric_for_best_model micro_f1 --greater_is_better True \
+    --shuffle_train_dataset --text_column_name text --label_column_name overall  --do_train --do_eval --do_predict --load_best_model_at_end --metric_for_best_model micro_f1 --greater_is_better True \
     --per_device_train_batch_size ${BATCH_SIZE} --per_device_eval_batch_size ${BATCH_SIZE} --gradient_accumulation_steps ${ACCUMULATION_STEPS} --eval_accumulation_steps ${ACCUMULATION_STEPS}
