902e8bfcb8a8e8816b59533a6bc7432b54cee790
weisi <weisi2580@gmail.com>
2023-12-04 05:45:09 -0600

    recoverd some files

--- baselines/amzHC_rating_run_all_by_seed.sh
+++ baselines/amzHC_rating_run_all_by_seed.sh
@@ -112,4 +112,3 @@ CUDA_VISIBLE_DEVICES=${GPU_NUMBER} python ${RUN_FILE} --model_name_or_path ${MOD
     --evaluation_strategy epoch --save_strategy epoch --save_total_limit 2 --max_seq_length 512 --learning_rate 3e-5 --num_train_epochs 25 \
     --shuffle_train_dataset --text_column_name text --label_column_name overall  --do_train --do_eval --do_predict --load_best_model_at_end --metric_for_best_model micro_f1 --greater_is_better True \
     --per_device_train_batch_size ${BATCH_SIZE} --per_device_eval_batch_size ${BATCH_SIZE} --gradient_accumulation_steps ${ACCUMULATION_STEPS} --eval_accumulation_steps ${ACCUMULATION_STEPS}
-
