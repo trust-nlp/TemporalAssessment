eef20f41cfb2680aea30aa9757749d1c44675857
weisi <weisi2580@gmail.com>
2023-12-04 18:35:54 -0600

    use new AY path (2007-2012) in AY_T4 experiment

--- baselines/amzHC_sentiment_run_all_by_seed.sh
+++ baselines/amzHC_sentiment_run_all_by_seed.sh
@@ -105,8 +105,8 @@ CUDA_VISIBLE_DEVICES=${GPU_NUMBER} python ${RUN_FILE} --model_name_or_path ${MOD
 
 CUDA_VISIBLE_DEVICES=${GPU_NUMBER} python ${RUN_FILE} --model_name_or_path ${MODEL_NAME} --seed 42\
     --output_dir logs/${TASK}/AY_T4/${MODEL_NAME}/split_seed${SPLITSEED}_model_seed_42_3e-5\
-    --train_file "$BASE_PATH/seed$SPLITSEED/amzHC_AY_2007-2014_train.json"\
-    --validation_file  "$BASE_PATH/seed$SPLITSEED/amzHC_AY_2007-2014_validation.json"\
+    --train_file "$BASE_PATH/seed$SPLITSEED/amzHC_AY_2007-2012_train.json"\
+    --validation_file  "$BASE_PATH/seed$SPLITSEED/amzHC_AY_2007-2012_validation.json"\
     --test_file  "$BASE_PATH/seed$SPLITSEED/amzHC_T4_2013-2014_test.json"\
     --evaluation_strategy epoch --save_strategy epoch --save_total_limit 2 --max_seq_length 512 --learning_rate 3e-5 --num_train_epochs 25 \
     --shuffle_train_dataset --text_column_name text --label_column_name sentiment  --do_train --do_eval --do_predict --load_best_model_at_end --metric_for_best_model micro_f1 --greater_is_better True \
