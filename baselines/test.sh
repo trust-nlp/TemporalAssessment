25c903cdc07d06e3943dc1ca6da68a0a676a18a3
weisi <weisi2580@gmail.com>
2023-12-04 18:37:16 -0600

    new experiment: re-run AY_T4 using new AY dataset

--- baselines/test.sh
+++ baselines/test.sh
@@ -9,10 +9,10 @@ RUN_FILE='/home/weisi/TemporalAssessment/baselines/classification.py'
 BASE_PATH='/home/weisi/TemporalAssessment/data/Amazon/HealthCare'
 
 CUDA_VISIBLE_DEVICES=${GPU_NUMBER} python ${RUN_FILE} --model_name_or_path ${MODEL_NAME} --seed 42\
-    --output_dir logs/${TASK}/T2_T3/${MODEL_NAME}/split_seed${SPLITSEED}_model_seed_42_3e-5\
-    --train_file "$BASE_PATH/seed$SPLITSEED/amzHC_T2_2009-2010_train.json"\
-    --validation_file  "$BASE_PATH/seed$SPLITSEED/amzHC_T2_2009-2010_validation.json"\
-    --test_file  "$BASE_PATH/seed$SPLITSEED/amzHC_T3_2011-2012_test.json"\
+    --output_dir logs/${TASK}/AY_T4/${MODEL_NAME}/split_seed${SPLITSEED}_model_seed_42_3e-5\
+    --train_file "$BASE_PATH/seed$SPLITSEED/amzHC_AY_2007-2012_train.json"\
+    --validation_file  "$BASE_PATH/seed$SPLITSEED/amzHC_AY_2007-2012_validation.json"\
+    --test_file  "$BASE_PATH/seed$SPLITSEED/amzHC_T4_2013-2014_test.json"\
     --evaluation_strategy epoch --save_strategy epoch --save_total_limit 2 --max_seq_length 512 --learning_rate 3e-5 --num_train_epochs 25 \
-    --shuffle_train_dataset --text_column_name text --label_column_name sentiment --do_train --do_eval --do_predict --load_best_model_at_end --metric_for_best_model micro_f1 --greater_is_better True \
+    --shuffle_train_dataset --text_column_name text --label_column_name sentiment  --do_train --do_eval --do_predict --load_best_model_at_end --metric_for_best_model micro_f1 --greater_is_better True \
     --per_device_train_batch_size ${BATCH_SIZE} --per_device_eval_batch_size ${BATCH_SIZE} --gradient_accumulation_steps ${ACCUMULATION_STEPS} --eval_accumulation_steps ${ACCUMULATION_STEPS}
