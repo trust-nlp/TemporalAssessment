8554e593be268e92fc28439f7617d774c49ca712
weisi <weisi2580@gmail.com>
2023-12-19 19:13:16 -0600

    added a record process program record_bioasq.py

--- baselines/BioASQ_factoid.sh
+++ baselines/BioASQ_factoid.sh
@@ -1,11 +1,11 @@
 GPU_NUMBER="1"  #"0,1"
 RUN_FILE='/home/weisi/TemporalAssessment/baselines/QA_seq2seq_factoid.py'
 BASE_PATH='/home/weisi/TemporalAssessment/data/BIOASQ'
-MODEL_NAME='razent/SciFive-base-Pubmed_PMC' #'razent/SciFive-base-Pubmed_PMC' 't5-base'
+MODEL_NAME='t5-base' #'razent/SciFive-base-Pubmed_PMC' 't5-base'
 BATCH_SIZE=32 #32?
 ACCUMULATION_STEPS=2
 SPLITSEED=1
-TASK='BioASQ_factoid_exact'
+TASK='BioASQ_factoid_ideal'
 #bioasq_factoid_T1_2013-2015_train.json
 #AY-T4 T1-T1 T1-T2 T1-T3 T1-T4 T2-T2 T2-T3 T2-T4 T3-T3 T3-T4 T4-T4 
 # T1_2013-2015 T2_2016-2018 T3_2019-2020 T4_2021-2022 AY_2013-2020
@@ -16,7 +16,7 @@ CUDA_VISIBLE_DEVICES=${GPU_NUMBER} python ${RUN_FILE} --model_name_or_path ${MOD
     --train_file "$BASE_PATH/seed$SPLITSEED/bioasq_factoid_AY_2013-2020_train.json"\
     --validation_file  "$BASE_PATH/seed$SPLITSEED/bioasq_factoid_AY_2013-2020_validation.json"\
     --test_file  "$BASE_PATH/seed$SPLITSEED/bioasq_factoid_T4_2021-2022_test.json"\
-    --context_column snippets  --question_column body  --answer_column exact_answer --do_train --do_eval --do_predict \
+    --context_column snippets  --question_column body  --answer_column ideal_answer --do_train --do_eval --do_predict \
     --predict_with_generate --evaluation_strategy epoch --learning_rate 3e-5 --max_seq_length 384 --max_answer_length 20 --doc_stride 128 --num_train_epochs 25 \
     --per_device_train_batch_size ${BATCH_SIZE} --per_device_eval_batch_size ${BATCH_SIZE} --gradient_accumulation_steps ${ACCUMULATION_STEPS} --eval_accumulation_steps ${ACCUMULATION_STEPS}
 
@@ -26,7 +26,7 @@ CUDA_VISIBLE_DEVICES=${GPU_NUMBER} python ${RUN_FILE} --model_name_or_path ${MOD
     --train_file "$BASE_PATH/seed$SPLITSEED/bioasq_factoid_T1_2013-2015_train.json"\
     --validation_file  "$BASE_PATH/seed$SPLITSEED/bioasq_factoid_T1_2013-2015_validation.json"\
     --test_file  "$BASE_PATH/seed$SPLITSEED/bioasq_factoid_T1_2013-2015_test.json"\
-    --context_column snippets  --question_column body  --answer_column exact_answer --do_train --do_eval --do_predict \
+    --context_column snippets  --question_column body  --answer_column ideal_answer --do_train --do_eval --do_predict \
     --predict_with_generate --evaluation_strategy epoch --learning_rate 3e-5 --max_seq_length 384 --max_answer_length 20 --doc_stride 128 --num_train_epochs 25 \
     --per_device_train_batch_size ${BATCH_SIZE} --per_device_eval_batch_size ${BATCH_SIZE} --gradient_accumulation_steps ${ACCUMULATION_STEPS} --eval_accumulation_steps ${ACCUMULATION_STEPS}
 
@@ -37,7 +37,7 @@ CUDA_VISIBLE_DEVICES=${GPU_NUMBER} python ${RUN_FILE} --model_name_or_path ${MOD
     --train_file "$BASE_PATH/seed$SPLITSEED/bioasq_factoid_T1_2013-2015_train.json"\
     --validation_file  "$BASE_PATH/seed$SPLITSEED/bioasq_factoid_T1_2013-2015_validation.json"\
     --test_file  "$BASE_PATH/seed$SPLITSEED/bioasq_factoid_T2_2016-2018_test.json"\
-    --context_column snippets  --question_column body  --answer_column exact_answer --do_train --do_eval --do_predict \
+    --context_column snippets  --question_column body  --answer_column ideal_answer --do_train --do_eval --do_predict \
     --predict_with_generate --evaluation_strategy epoch --learning_rate 3e-5 --max_seq_length 384 --max_answer_length 20 --doc_stride 128 --num_train_epochs 25 \
     --per_device_train_batch_size ${BATCH_SIZE} --per_device_eval_batch_size ${BATCH_SIZE} --gradient_accumulation_steps ${ACCUMULATION_STEPS} --eval_accumulation_steps ${ACCUMULATION_STEPS}
 
@@ -48,7 +48,7 @@ CUDA_VISIBLE_DEVICES=${GPU_NUMBER} python ${RUN_FILE} --model_name_or_path ${MOD
     --train_file "$BASE_PATH/seed$SPLITSEED/bioasq_factoid_T1_2013-2015_train.json"\
     --validation_file  "$BASE_PATH/seed$SPLITSEED/bioasq_factoid_T1_2013-2015_validation.json"\
     --test_file  "$BASE_PATH/seed$SPLITSEED/bioasq_factoid_T3_2019-2020_test.json"\
-    --context_column snippets  --question_column body  --answer_column exact_answer --do_train --do_eval --do_predict \
+    --context_column snippets  --question_column body  --answer_column ideal_answer --do_train --do_eval --do_predict \
     --predict_with_generate --evaluation_strategy epoch --learning_rate 3e-5 --max_seq_length 384 --max_answer_length 20 --doc_stride 128 --num_train_epochs 25 \
     --per_device_train_batch_size ${BATCH_SIZE} --per_device_eval_batch_size ${BATCH_SIZE} --gradient_accumulation_steps ${ACCUMULATION_STEPS} --eval_accumulation_steps ${ACCUMULATION_STEPS}
 
@@ -58,7 +58,7 @@ CUDA_VISIBLE_DEVICES=${GPU_NUMBER} python ${RUN_FILE} --model_name_or_path ${MOD
     --train_file "$BASE_PATH/seed$SPLITSEED/bioasq_factoid_T1_2013-2015_train.json"\
     --validation_file  "$BASE_PATH/seed$SPLITSEED/bioasq_factoid_T1_2013-2015_validation.json"\
     --test_file  "$BASE_PATH/seed$SPLITSEED/bioasq_factoid_T4_2021-2022_test.json"\
-    --context_column snippets  --question_column body  --answer_column exact_answer --do_train --do_eval --do_predict \
+    --context_column snippets  --question_column body  --answer_column ideal_answer --do_train --do_eval --do_predict \
     --predict_with_generate --evaluation_strategy epoch --learning_rate 3e-5 --max_seq_length 384 --max_answer_length 20 --doc_stride 128 --num_train_epochs 25 \
     --per_device_train_batch_size ${BATCH_SIZE} --per_device_eval_batch_size ${BATCH_SIZE} --gradient_accumulation_steps ${ACCUMULATION_STEPS} --eval_accumulation_steps ${ACCUMULATION_STEPS}
 
@@ -68,7 +68,7 @@ CUDA_VISIBLE_DEVICES=${GPU_NUMBER} python ${RUN_FILE} --model_name_or_path ${MOD
     --train_file "$BASE_PATH/seed$SPLITSEED/bioasq_factoid_T2_2016-2018_train.json"\
     --validation_file  "$BASE_PATH/seed$SPLITSEED/bioasq_factoid_T2_2016-2018_validation.json"\
     --test_file  "$BASE_PATH/seed$SPLITSEED/bioasq_factoid_T2_2016-2018_test.json"\
-    --context_column snippets  --question_column body  --answer_column exact_answer --do_train --do_eval --do_predict \
+    --context_column snippets  --question_column body  --answer_column ideal_answer --do_train --do_eval --do_predict \
     --predict_with_generate --evaluation_strategy epoch --learning_rate 3e-5 --max_seq_length 384 --max_answer_length 20 --doc_stride 128 --num_train_epochs 25 \
     --per_device_train_batch_size ${BATCH_SIZE} --per_device_eval_batch_size ${BATCH_SIZE} --gradient_accumulation_steps ${ACCUMULATION_STEPS} --eval_accumulation_steps ${ACCUMULATION_STEPS}
 
@@ -78,7 +78,7 @@ CUDA_VISIBLE_DEVICES=${GPU_NUMBER} python ${RUN_FILE} --model_name_or_path ${MOD
     --train_file "$BASE_PATH/seed$SPLITSEED/bioasq_factoid_T2_2016-2018_train.json"\
     --validation_file  "$BASE_PATH/seed$SPLITSEED/bioasq_factoid_T2_2016-2018_validation.json"\
     --test_file  "$BASE_PATH/seed$SPLITSEED/bioasq_factoid_T3_2019-2020_test.json"\
-    --context_column snippets  --question_column body  --answer_column exact_answer --do_train --do_eval --do_predict \
+    --context_column snippets  --question_column body  --answer_column ideal_answer --do_train --do_eval --do_predict \
     --predict_with_generate --evaluation_strategy epoch --learning_rate 3e-5 --max_seq_length 384 --max_answer_length 20 --doc_stride 128 --num_train_epochs 25 \
     --per_device_train_batch_size ${BATCH_SIZE} --per_device_eval_batch_size ${BATCH_SIZE} --gradient_accumulation_steps ${ACCUMULATION_STEPS} --eval_accumulation_steps ${ACCUMULATION_STEPS}
 
@@ -88,7 +88,7 @@ CUDA_VISIBLE_DEVICES=${GPU_NUMBER} python ${RUN_FILE} --model_name_or_path ${MOD
     --train_file "$BASE_PATH/seed$SPLITSEED/bioasq_factoid_T2_2016-2018_train.json"\
     --validation_file  "$BASE_PATH/seed$SPLITSEED/bioasq_factoid_T2_2016-2018_validation.json"\
     --test_file  "$BASE_PATH/seed$SPLITSEED/bioasq_factoid_T4_2021-2022_test.json"\
-    --context_column snippets  --question_column body  --answer_column exact_answer --do_train --do_eval --do_predict \
+    --context_column snippets  --question_column body  --answer_column ideal_answer --do_train --do_eval --do_predict \
     --predict_with_generate --evaluation_strategy epoch --learning_rate 3e-5 --max_seq_length 384 --max_answer_length 20 --doc_stride 128 --num_train_epochs 25 \
     --per_device_train_batch_size ${BATCH_SIZE} --per_device_eval_batch_size ${BATCH_SIZE} --gradient_accumulation_steps ${ACCUMULATION_STEPS} --eval_accumulation_steps ${ACCUMULATION_STEPS}
 
@@ -98,7 +98,7 @@ CUDA_VISIBLE_DEVICES=${GPU_NUMBER} python ${RUN_FILE} --model_name_or_path ${MOD
     --train_file "$BASE_PATH/seed$SPLITSEED/bioasq_factoid_T3_2019-2020_train.json"\
     --validation_file  "$BASE_PATH/seed$SPLITSEED/bioasq_factoid_T3_2019-2020_validation.json"\
     --test_file  "$BASE_PATH/seed$SPLITSEED/bioasq_factoid_T3_2019-2020_test.json"\
-    --context_column snippets  --question_column body  --answer_column exact_answer --do_train --do_eval --do_predict \
+    --context_column snippets  --question_column body  --answer_column ideal_answer --do_train --do_eval --do_predict \
     --predict_with_generate --evaluation_strategy epoch --learning_rate 3e-5 --max_seq_length 384 --max_answer_length 20 --doc_stride 128 --num_train_epochs 25 \
     --per_device_train_batch_size ${BATCH_SIZE} --per_device_eval_batch_size ${BATCH_SIZE} --gradient_accumulation_steps ${ACCUMULATION_STEPS} --eval_accumulation_steps ${ACCUMULATION_STEPS}
 
@@ -108,7 +108,7 @@ CUDA_VISIBLE_DEVICES=${GPU_NUMBER} python ${RUN_FILE} --model_name_or_path ${MOD
     --train_file "$BASE_PATH/seed$SPLITSEED/bioasq_factoid_T3_2019-2020_train.json"\
     --validation_file  "$BASE_PATH/seed$SPLITSEED/bioasq_factoid_T3_2019-2020_validation.json"\
     --test_file  "$BASE_PATH/seed$SPLITSEED/bioasq_factoid_T4_2021-2022_test.json"\
-    --context_column snippets  --question_column body  --answer_column exact_answer --do_train --do_eval --do_predict \
+    --context_column snippets  --question_column body  --answer_column ideal_answer --do_train --do_eval --do_predict \
     --predict_with_generate --evaluation_strategy epoch --learning_rate 3e-5 --max_seq_length 384 --max_answer_length 20 --doc_stride 128 --num_train_epochs 25 \
     --per_device_train_batch_size ${BATCH_SIZE} --per_device_eval_batch_size ${BATCH_SIZE} --gradient_accumulation_steps ${ACCUMULATION_STEPS} --eval_accumulation_steps ${ACCUMULATION_STEPS}
 
@@ -118,7 +118,7 @@ CUDA_VISIBLE_DEVICES=${GPU_NUMBER} python ${RUN_FILE} --model_name_or_path ${MOD
     --train_file "$BASE_PATH/seed$SPLITSEED/bioasq_factoid_T4_2021-2022_train.json"\
     --validation_file  "$BASE_PATH/seed$SPLITSEED/bioasq_factoid_T4_2021-2022_validation.json"\
     --test_file  "$BASE_PATH/seed$SPLITSEED/bioasq_factoid_T4_2021-2022_test.json"\
-    --context_column snippets  --question_column body  --answer_column exact_answer --do_train --do_eval --do_predict \
+    --context_column snippets  --question_column body  --answer_column ideal_answer --do_train --do_eval --do_predict \
     --predict_with_generate --evaluation_strategy epoch --learning_rate 3e-5 --max_seq_length 384 --max_answer_length 20 --doc_stride 128 --num_train_epochs 25 \
     --per_device_train_batch_size ${BATCH_SIZE} --per_device_eval_batch_size ${BATCH_SIZE} --gradient_accumulation_steps ${ACCUMULATION_STEPS} --eval_accumulation_steps ${ACCUMULATION_STEPS}
 
---
+++ record_bioasq.py
@@ -0,0 +1,31 @@
+import json
+import os
+import pandas as pd
+
+# paths
+base_dir = '/home/weisi/TemporalAssessment/logs/BioASQ_factoid_exact'
+options = ['T1-T1', 'T1-T2', 'T1-T3', 'T1-T4', 'T2-T2', 'T2-T3', 'T2-T4', 'T3-T3', 'T3-T4', 'T4-T4', 'AY-T4']
+file_name = 'predict_results.json'
+out_csv_file_name = 'BioASQ_factoid_exactans_scores_t5-base_seed1.csv'
+
+results = {}
+
+#razent/SciFive-base-Pubmed_PMC
+#'t5-base'
+#/home/weisi/TemporalAssessment/logs/BioASQ_factoid_exact/T1_T1/t5-base/split_seed1/model_seed_42_3e-5_maxanslen20
+for option in options:
+    file_path = os.path.join(base_dir, option.replace('-', '_'), 't5-base', 'split_seed1','model_seed_42_3e-5_maxanslen20', file_name)
+    if os.path.exists(file_path):
+        with open(file_path, 'r') as file:
+            data = json.load(file)
+            bleu_score = data['test_bleu']['bleu']
+            precisions = data['test_bleu']['precisions'] # 4 n gram precision
+            exact_match = data['test_exact_match']['exact_match']
+            google_bleu = data['test_google_bleu']['google_bleu']
+            rouge_scores = data['test_rouge']
+            # add to result
+            results[option] = [bleu_score] + precisions + [exact_match, google_bleu] + list(rouge_scores.values())
+
+
+df = pd.DataFrame.from_dict(results, orient='index', columns=['bleu', '1 precision', '2 precision', '3 precision', '4 precision', 'exact_match', 'google_bleu', 'rouge1', 'rouge2', 'rougeL', 'rougeLsum'])
+df.to_csv(os.path.join(base_dir,out_csv_file_name))
